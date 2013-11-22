105cd8ed9666cc8933f3a658c2ec1d54b28a1dc4
srsinha <srsinha@indiana.edu>
2013-11-21 19:11:00 -0500

    Added DictDiffer (looks for differences between two dict), and a test Microsoft Excel spreadsheet. Also pushed the correct version of MM_Dispenser.py. This one includes the use of DictDiffere.py & serial output.

--- code/MM_Dispenser.py
+++ code/MM_Dispenser.py
@@ -1,109 +1,124 @@
-import time, datetime
+#!usr/bin/env
+"""
+M&M Dispenser Host Computer Program
+This program interfaces with a Microsoft Excel spreadsheet & communicates signals to an Arduino via serial interface.
+
+MS Excel communication is made possible using the XLRD library
+Serial commuincation is possible using the PySerial library
+
+Current version uses dict to represent represent MS Excel spreadsheet contents
+See DictDiffer library
+"""
+
+import time
 import xlrd
+import serial
+import copy
+from DictDiffer import *
 
 ## Define parameters
-Loop_time = 5  # The time in seconds to wait before scanning for completed tasks
-#prev_dict = {}
-#curr_dict = {}
+Loop_time = 3  # The time in seconds to wait before scanning for completed tasks
+prev_dict = {}
+curr_dict = {}
+
+## Configure serial (USB) line
+ser = serial.Serial('/dev/tty.usbserial-A700eY2N',9600,timeout=1)        
+print ser.portstr
 
 ### Open the pre-defined Microsoft Excel spreadsheet 
-
-todo_spreadsheet = "MM_Dispenser_Test.xls"
-book = xlrd.open_workbook(todo_spreadsheet, formatting_info=1) 
+book = xlrd.open_workbook("/Users/Shiva/Documents/Life/Hobbies/Arduino/Candy_Dispenser/MM_Dispenser_Test_Short.xls", formatting_info=1) 
 sheets = book.sheet_names()
 print "The sheet names are:", sheets 
-#book.release_resources()
 
-def get_current_state():
-    """
-    read the excel spreadsheet into a native python format
-    """
-    #seems like a list of lists (or a list of lists of lists) (sheets, rows, columns)
-    #should be sufficient
-    #this also gives order data, in case that is ever needed
-    current_state = []
 
-    #need to re-open every time to make sure we're getting the latest version
-    book = xlrd.open_workbook(todo_spreadsheet, formatting_info=1) 
-    
-    sheets = book.sheet_names()
-    for index, sh in enumerate(sheets): 
-        sheet = book.sheet_by_index(index) 
-        #print "\nSheet name:", sheet.name 
-        rows, cols = sheet.nrows, sheet.ncols 
-        #print "Number of rows: %s   Number of cols: %s" % (rows, cols)
-        current_rows = []
-        for row in range(rows):
-            thecell = sheet.cell(row, 1)
-            xfx = sheet.cell_xf_index(row, 2)
-            xf = book.xf_list[xfx]
-            bgx = xf.background.pattern_colour_index
-            current_cols = [ thecell.value, bgx ]
-            current_rows.append(current_cols)
-        current_state.append(current_rows)
+### First time the program runs, collect the current state of the MS Excel ssheet
+for index, sh in enumerate(sheets): 
+    sheet = book.sheet_by_index(index) 
+    print "\nSheet name:", sheet.name 
+    rows, cols = sheet.nrows, sheet.ncols 
+    print "Number of rows: %s   Number of cols: %s" % (rows, cols) 
+    for row in range(rows): 
+        for col in range(cols):
+            ## print "row, col is:", row+1, col+1,
+            if (col+1) == 2:
+                thecell = sheet.cell(row, col)  # could get 'dump', 'value', 'xf_index'
+                print thecell.value,
+                prev_dict[thecell.value]=0
 
-    #book.release_resources()
-    return current_state
-
-def compare_states(prev_state, new_state):
-    for sheet_index in range(len(prev_state)):
-        for row_index in range(len(prev_state[sheet_index])):
-            #check if the row is still in the new_state:
-            prev_row = prev_state[sheet_index][row_index]
-            if prev_row in new_state[sheet_index]:
-                new_index = new_state[sheet_index].index( prev_row )
-                if new_index == row_index:
-                    #position and row is the same, nothing to see here
-                    pass
-                else:
-                    print "Noticed that: %s moved from position: %s to position: %s" % (prev_row, row_index, new_index)
-            else:
-                #can't find the exact same task / color combination in the current sheet
-                #check if the color has changed
-                matched = False
-                for new_row in new_state[sheet_index]:
-                    if new_row[0] == prev_row[0]:
-                        print "Found color change for: %s to: %s" % (prev_row, new_row)
-                        matched = True
-
-                if not matched:
-                    print "Couldn't find: %s in current spread sheet (new_state). deleted?" % prev_row
-                    
-                # this only checks for items missing from new_state
-                # does not signal for new rows added to new_state
-    
-prev_state = get_current_state()
-print prev_state
+            elif (col+1) == 3:
+                xfx = sheet.cell_xf_index(row, col) 
+                xf = book.xf_list[xfx] 
+                bgx = xf.background.pattern_colour_index
+                print bgx
+                prev_dict[thecell.value]=bgx
 
 print '\nCompleted reading contents for current Task List\n'
 
+    
 ### Continually loop every 'Loop_time' seconds, and scan for compeleted tasks, unless interrupted
 while True:
     try: ## The code below will be executed every N seconds
 
-        new_state = get_current_state()
-        #print new_state
+        ## Pause before repeating search for completed tasks
+        print '\nPausing before repeating search for changes\n'
+        time.sleep(Loop_time)
+
+        book = xlrd.open_workbook("/Users/Shiva/Documents/Life/Hobbies/Arduino/Candy_Dispenser/MM_Dispenser_Test_Short.xls", formatting_info=1) 
+        sheets = book.sheet_names()
+        sheet = book.sheet_by_index(0) 
+        rows, cols = sheet.nrows, sheet.ncols 
+
+        ## Read in the current entries from the MS Excel spreadsheet
+        for index, sh in enumerate(sheets): 
+            sheet = book.sheet_by_index(index) 
+            print "Sheet name:", sheet.name 
+            rows, cols = sheet.nrows, sheet.ncols 
+            print "Number of rows: %s   Number of cols: %s" % (rows, cols) 
+            for row in range(rows): 
+                for col in range(cols):
+                    if (col+1) == 2:
+                        thecell = sheet.cell(row, col)  # could get 'dump', 'value', 'xf_index'
+                        if thecell.value:
+                            print thecell.value,
+                        elif empty_cell(thecell.value):
+                            print '-',
+                        curr_dict[thecell.value]=0
+
+                    elif (col+1) == 3:
+                        xfx = sheet.cell_xf_index(row, col) 
+                        xf = book.xf_list[xfx] 
+                        bgx = xf.background.pattern_colour_index
+                        print bgx
+                        curr_dict[thecell.value]=bgx
 
         ## Compare the prev_dict with the curr_dict and identify a completed task
         ## Take one curr_dict entry at a time, search for it in the prev_dict, ...
         ##      and see if the pattern_colour_index has changed 
-        compare_states(prev_state, new_state)
+        myClass = DictDiffer(curr_dict,prev_dict)
+        changedSet = myClass.changed()
+        if (len(changedSet) > 0):
+            print changedSet
+            for i in changedSet:
+                print 'current_dict value: ', curr_dict[i]
+                print 'previous_dict value: ', prev_dict[i]
+                ## Output the task value to the serial line
+                x = ser.write(str(curr_dict[i]))
 
-        #be sure to update... don't want to dispense more than deserved amount! :)
-        prev_state = new_state
+            prev_dict.clear()
+            prev_dict = copy.deepcopy(curr_dict)
+            curr_dict.clear()
+            print "A new task has been completed."
 
-        ## Pause before repeating search for completed tasks
-        print '\nPausing before repeating search for changes (%s) \n' % (datetime.datetime.now())
+        else:
+            print "No new task completed."
 
-        time.sleep(Loop_time)
 
     ## When the Ctrl+C keystroke is made the program terminates 
     except (KeyboardInterrupt, SystemExit):
-        print '\nProgram terminated! No traceback to see here. Get back to work!\n'
+        print '\nProgram terminated! Need to figure out how to terminate without Traceback!!\n'
         break
 
     ## Print statements and complete any final housekeeping chores before halting program execution 
     finally:
+        ser.close
         pass
-
-
